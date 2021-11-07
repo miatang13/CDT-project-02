7725ef38eb2b4ba7bfdf23d3ebdd3ac739fcbcc4
miatang13 <miatang13@gmail.com>
2021-11-07 01:40:23 -0500

    Fetches from rapidapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch(
+      "https://twinword-text-classification.p.rapidapi.com/classify/?text=Hihihih",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "twinword-text-classification.p.rapidapi.com",
+          "x-rapidapi-key":
+            "027cff5cbfmsh439002e33664b4ap1c34c7jsn8622e3b4d4ca",
+        },
+      }
+    )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
