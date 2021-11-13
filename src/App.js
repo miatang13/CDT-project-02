bb4011c95f6830c10d56dd276512caf575597381
miatang13 <miatang13@gmail.com>
2021-11-13 17:42:53 -0500

    Got data for index 0-200 for starting 2010

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useEffect, useRef, useState } from "react";
-import data from "./data/bechdel-test-test.json";
+import data from "./data/bechdel-test-2010-onwards.json";
+//import data from "./data/bechdel-test-test.json";
 
 function App() {
   const btnRef = useRef();
   const [downloadHref, setDownloadHref] = useState();
   const [dataJson, setData] = useState([]);
-  const [fetchedNum, setFetchedNum] = useState(0);
 
   const outputJson = () => {
     //https://stackoverflow.com/questions/65903776/how-to-read-and-write-to-local-json-files-from-react-js
@@ -42,7 +42,6 @@ function App() {
         let orig = dataJson;
         orig.push(data);
         setData(orig);
-        setFetchedNum(fetchedNum + 1);
       })
       .catch((err) => {
         console.error(err);
@@ -50,37 +49,12 @@ function App() {
   };
 
   const fetchAllData = () => {
-    data.forEach((val) => {
-      fetchData(val.imdbid);
-    });
-  };
-
-  useEffect(() => {
-    if (fetchedNum === data.length) {
-      console.log("Finished fetching data");
+    const startIdx = 0;
+    const endIdx = Math.min(200, data.length);
+    for (let i = startIdx; i < endIdx; i++) {
+      fetchData(data[i].imdbid);
     }
-  }, [fetchedNum]);
-
-  /*
-  useEffect(() => {
-    fetch(
-      "https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt8858104&r=json&page=1",
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-          "x-rapidapi-key":
-            "027cff5cbfmsh439002e33664b4ap1c34c7jsn8622e3b4d4ca",
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
-  */
+  };
 
   return (
     <div className="App">
