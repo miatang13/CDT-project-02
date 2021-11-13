c27266c346a7218d9600d651f273f1e6d0bcfab1
miatang13 <miatang13@gmail.com>
2021-11-13 16:10:25 -0500

    Working fetch and search by imdb

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import "./App.css";
 import { useEffect } from "react";
+import data from "./data/bechdel-test-all.json";
 
 function App() {
+  console.log(data.length);
+
   useEffect(() => {
-    fetch("https://imdb8.p.rapidapi.com/auto-complete?q=twilight", {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "imdb8.p.rapidapi.com",
-        "x-rapidapi-key": "027cff5cbfmsh439002e33664b4ap1c34c7jsn8622e3b4d4ca",
-      },
-    })
-      .then((response) => {
-        console.log(response.body);
-      })
+    fetch(
+      "https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt8858104&r=json&page=1",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+          "x-rapidapi-key":
+            "027cff5cbfmsh439002e33664b4ap1c34c7jsn8622e3b4d4ca",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
       .catch((err) => {
         console.error(err);
       });
   }, []);
+
   return (
     <div className="App">
       <header className="App-header"></header>
