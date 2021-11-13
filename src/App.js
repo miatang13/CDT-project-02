e642ffc14569bbaa0b54ebff4576f4124746aea2
miatang13 <miatang13@gmail.com>
2021-11-13 16:56:52 -0500

    Update json for download with fetch data

--- src/App.js
+++ src/App.js
@@ -4,22 +4,52 @@ import data from "./data/bechdel-test-selected.json";
 function App() {
   const btnRef = useRef();
   const [downloadHref, setDownloadHref] = useState();
+  const [dataJson, setData] = useState([{ hi: "okay" }]);
 
-  useEffect(() => {
-    console.log(data.length);
-
+  const outputJson = () => {
     //https://stackoverflow.com/questions/65903776/how-to-read-and-write-to-local-json-files-from-react-js
     const TextFile = () => {
-      const textFile = new Blob(
-        [[JSON.stringify("pass data from localStorage")]],
-        { type: "application/json" }
-      ); //pass data from localStorage API to blob
+      const textFile = new Blob([[JSON.stringify(dataJson)]], {
+        type: "application/json",
+      }); //pass data from localStorage API to blob
       setDownloadHref(URL.createObjectURL(textFile));
       btnRef.current.download = "data.json";
     };
 
     TextFile();
+  };
+
+  useEffect(() => {
+    console.log(data.length);
   }, []);
+
+  const fetchData = (imdbid) => {
+    let fetchLink =
+      "https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt" +
+      imdbid +
+      "&r=json&page=1";
+    fetch(fetchLink, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+        "x-rapidapi-key": "027cff5cbfmsh439002e33664b4ap1c34c7jsn8622e3b4d4ca",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let orig = dataJson;
+        orig.push(data);
+        setData(orig);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const fetchAllData = () => {
+    fetchData("0199753");
+  };
 
   /*
   useEffect(() => {
@@ -45,10 +75,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <a ref={btnRef} href={downloadHref}>
+      <a ref={btnRef} href={downloadHref} onClick={outputJson}>
         {" "}
         Download file{" "}
       </a>
+      <button onClick={fetchAllData}> fetch data</button>
     </div>
   );
 }
