c204d9f06595ceebdb8c64f25d2cbb6b249c3062
miatang13 <miatang13@gmail.com>
2021-12-05 11:36:35 -0500

    Fixed no scroll bug

--- src/pages/Analysis.js
+++ src/pages/Analysis.js
@@ -23,59 +23,65 @@ export default function Analysis() {
   }
 
   return (
-    <div className="root" id="analysis__root">
+    <div id="analysis__root">
       <NavigationBar color="0x000000" />
 
-      <div id="analysis__content">
+      <div id="analysis__content" className="min-vh-100">
         <h1 id="title">Your top directors</h1>
 
         {cartData.length === 0 && <EmptyAnalysis />}
 
         {cartData.length !== 0 && (
-          <Container className="directors__wrapper" fluid>
-            <Row className="justify-content-md-center">
-              {cartData.map((item, index) => (
-                <Col key={item.name + index}>
-                  <Card style={{ width: "15rem" }} bg="light">
-                    <Card.Body>
-                      <Card.Title className="directorName">
-                        {item.name}
-                      </Card.Title>
-                      <Card.Text>{item.movieCnt} passing movies</Card.Text>
-                      <Carousel variant="dark" indicators={false}>
-                        {item.movies.map((movieObj) => (
-                          <Carousel.Item key={movieObj.Title}>
-                            <img
-                              className="d-block w-100"
-                              src={movieObj.Poster}
-                              alt="First slide"
-                              style={{ marginBottom: "1vh" }}
-                            />
-                            <h4 className="movieTitle">{movieObj.Title}</h4>
-                            <p className="movieBoxoffice roundedBg">
-                              💰💰 {movieObj.BoxOffice}
-                            </p>
-                            <p className="movieAwards roundedBg">
-                              🏆🏆 {movieObj.Awards}
-                            </p>
-                            <p className="movieGenre">{movieObj.Genre}</p>
-                          </Carousel.Item>
-                        ))}
-                      </Carousel>
-                    </Card.Body>
-                    <Card.Footer>
-                      <Button
-                        variant="outline-secondary"
-                        onClick={() => handleRemove(item.name)}
-                      >
-                        Remove
-                      </Button>
-                    </Card.Footer>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-          </Container>
+          <div>
+            <Container id="directors__wrapper" fluid>
+              <Row className="justify-content-md-center">
+                {cartData.map((item, index) => (
+                  <Col key={item.name + index}>
+                    <Card style={{ width: "15rem" }} bg="light">
+                      <Card.Body>
+                        <Card.Title className="directorName">
+                          {item.name}
+                        </Card.Title>
+                        <Card.Text>{item.movieCnt} passing movies</Card.Text>
+                        <Carousel variant="dark" indicators={false}>
+                          {item.movies.map((movieObj) => (
+                            <Carousel.Item key={movieObj.Title}>
+                              <img
+                                className="d-block w-100"
+                                src={movieObj.Poster}
+                                alt="First slide"
+                                style={{ marginBottom: "1vh", height: "350px" }}
+                              />
+                              <h4 className="movieTitle">{movieObj.Title}</h4>
+                              <p className="movieBoxoffice roundedBg">
+                                💰💰 {movieObj.BoxOffice}
+                              </p>
+                              <p className="movieAwards roundedBg">
+                                🏆🏆 {movieObj.Awards}
+                              </p>
+                              <p className="movieGenre">{movieObj.Genre}</p>
+                            </Carousel.Item>
+                          ))}
+                        </Carousel>
+                      </Card.Body>
+                      <Card.Footer>
+                        <Button
+                          variant="outline-secondary"
+                          onClick={() => handleRemove(item.name)}
+                        >
+                          Remove
+                        </Button>
+                      </Card.Footer>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </Container>
+            <div>
+              {" "}
+              <h1> Recommended </h1>
+            </div>
+          </div>
         )}
       </div>
     </div>
