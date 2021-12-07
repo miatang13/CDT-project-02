3bc5ca28d11ac2c451e57552d621213541e03131
miatang13 <miatang13@gmail.com>
2021-12-07 00:25:31 -0500

    Change shuffle wording

--- src/pages/Analysis.js
+++ src/pages/Analysis.js
@@ -189,7 +189,20 @@ export default function Analysis() {
               style={{ textAlign: "center" }}
             >
               <button style={buttonStyle} onClick={handleChangeRec}>
-                Get 3 Other Random Directors{" "}
+                Shuffle Suggestions{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-left-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z"
+                  />
+                </svg>
               </button>
               <h1 className="sub__title">You May Also Like:</h1>
               <Row>
@@ -199,7 +212,6 @@ export default function Analysis() {
                     pathname: "/",
                     directorIdx: rec.unsortedIndex,
                   };
-                  console.log("hi", newTo.directorIdx);
 
                   return (
                     <Col key={rec.name}>
